d1076b01878e572344dc286088d0e0434ab92aeb
NMTHmations <125398293+NMTHmations@users.noreply.github.com>
2025-03-08 21:27:03 +0100

    Still empty project, but it is java, not kotlin!

---
+++ app/src/main/java/com/example/pebblethrower/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.pebblethrower;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.pebblethrower.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'pebblethrower' library on application startup.
+    static {
+        System.loadLibrary("pebblethrower");
+    }
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Example of a call to a native method
+        TextView tv = binding.sampleText;
+        tv.setText(stringFromJNI());
+    }
+
+    /**
+     * A native method that is implemented by the 'pebblethrower' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
