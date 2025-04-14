d0caa27a689a5b4d4b7ee97605c5a2073be312ac
NMTHmations <125398293+NMTHmations@users.noreply.github.com>
2025-04-14 13:04:06 +0200

    Switch added to leaderboard

--- app/src/main/java/com/example/pebblethrower/Leaderboard.java
+++ app/src/main/java/com/example/pebblethrower/Leaderboard.java
@@ -1,9 +1,9 @@
 package com.example.pebblethrower;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,12 +12,11 @@ import androidx.room.Room;
 import com.example.pebblethrower.databinding.LeaderboardBinding;
 import com.example.pebblethrower.model.AppDatabase;
 import com.example.pebblethrower.model.User;
-import com.example.pebblethrower.model.UserDAO;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Leaderboard extends AppCompatActivity {
+    StringBuilder result = new StringBuilder();
 
     public User createUser(int uint){
         User user = new User();
@@ -34,16 +33,13 @@ public class Leaderboard extends AppCompatActivity {
         setContentView(binding.getRoot());
         AppDatabase db = Room.databaseBuilder(getApplicationContext(),
                 AppDatabase.class, "database-name").allowMainThreadQueries().build(); // not recommended, but only works in this way
-        //User user = new User(getIntent().getStringExtra("NAME"),getIntent().getFloatExtra("VELOCITY",0.0f));
-        List<User> userList =  db.userDao().getAll();
+        List<User> userList = db.userDao().getAll();
         int number = userList.size() + 1;
         User user = createUser(number);
         db.userDao().Insert(user);
-        StringBuilder result = new StringBuilder();
         userList = db.userDao().getAll();
-        for (var i : userList)
-        {
-            result.append("Name: "+i.getName()+", Velocity: "+i.getMax_velocity() + "\n");
+        for (var i : userList) {
+            result.append("Name: " + i.getName() + ", Velocity: " + i.getMax_velocity() + "\n");
         }
         TextView tv = findViewById(R.id.textView4);
         tv.setText(result);
@@ -59,5 +55,20 @@ public class Leaderboard extends AppCompatActivity {
         Intent intent = new Intent(Leaderboard.this,MainActivity.class);
         startActivity(intent);
         finish();
+    }
+
+    public void switched(View view)
+    {
+        //User user = new User(getIntent().getStringExtra("NAME"),getIntent().getFloatExtra("VELOCITY",0.0f));
+        Switch switch_controller = (Switch) findViewById(R.id.switch1);
+        if (!switch_controller.isChecked()) {
+            TextView tv = findViewById(R.id.textView4);
+            tv.setText(result);
+        }
+        else
+        {
+            TextView tv = findViewById(R.id.textView4);
+            tv.setText("The online leaderboard will show up here");
+        }
     }
 }
